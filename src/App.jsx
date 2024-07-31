b556c9c9eb611f6a51f40bd4f3cd01ac1ba33958
_Joshi123 <aayushj2001@gmail.com>
2024-07-31 21:14:06 +0545

    feat: new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
 import Experience from "./components/Experience.jsx";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Project from "./components/Project.jsx";
 import Technologies from "./components/Technologies.jsx";
 
 const App = () => {
@@ -17,6 +19,8 @@ const App = () => {
         <About />
         <Technologies />
         <Experience />
+        <Project />
+        <Contact />
       </div>
     </div>
   );
